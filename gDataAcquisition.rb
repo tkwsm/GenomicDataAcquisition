fdfc0d1816053e220d7fe31e76d3a3c3cde5f03c
Tkwsm <kawashima38@gmail.com>
2021-07-08 16:50:13 +0900

    modify gDataAcquisition.rb

--- gDataAcquisition.rb
+++ gDataAcquisition.rb
@@ -1,28 +1,53 @@
 #!/usr/bin/ruby
 
-def getData( url, outputdir )
+def getData( url, outputdir, subdir )
 
   STDERR.puts "Start getData"
   fname = "-"; fname_mod1 = "-"; fname_mod2 = "-";
   fname      = url.chomp.split("\/")[-1]
   fname_mod1 = fname.slice(/^(\S+).tar.gz/, 1) if fname =~ /.tar.gz/
   fname_mod2 = fname.slice(/^(\S+).gz/, 1)     if fname =~ /.gz/
-  if    File.exist?( "#{outputdir}/#{fname}")
-  elsif File.exist?( "#{outputdir}/#{fname_mod1}")
-  elsif File.exist?( "#{outputdir}/#{fname_mod2}")
+
+  if    File.exist?( "#{outputdir}")
   else
-    `wget -o "#{outputdir}/#{fname}" "#{url}" `
+    ` mkdir #{outputdir}`
+  end
+
+  if    File.exist?( "#{outputdir}/#{subdir}")
+  else
+    ` mkdir #{outputdir}/#{subdir} `
+  end
+
+  if    File.exist?( "#{outputdir}/#{subdir}/#{fname}")
+  elsif File.exist?( "#{outputdir}/#{subdir}/#{fname_mod1}")
+  elsif File.exist?( "#{outputdir}/#{subdir}/#{fname_mod2}")
+  else
+    ` wget -P "#{outputdir}/#{subdir}" "#{url}" `
   end
 
 end
 
-def parse_list( list4url_f, dir4download )
+def parse_list( list4url_f, list4taxonomy_f, dir4download )
+
+  outputdir_h = {}
+
+  open( list4taxonomy_f ).each do |x|
+    a = x.chomp.split("\t")
+    data_id = a[0]
+    tax_id  = a[1]
+    dirname = a[2]
+    outputdir_h[ data_id ] = dirname
+  end
+
   open( list4url_f ).each do |x|
     next if x =~ /^\#/
     a = x.chomp.split("\t")
-    url = a[4]
-    getData( url, dir4download )
+    data_id = a[1]
+    url     = a[4]
+    subdirname = outputdir_h[ data_id ]
+    getData( url, dir4download, subdirname )
   end
+
 end
 
 ######################################################
@@ -33,5 +58,5 @@ list4url_f      = ARGV.shift
 list4taxonomy_f = ARGV.shift
 dir4download    = ARGV.shift
 
-parse_list( list4url_f, dir4download )
+parse_list( list4url_f, list4taxonomy_f, dir4download )
 
